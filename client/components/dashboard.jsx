d687cfc959ebba5aea11343c1fa5f9e7d3a6c1fd
anjaleena <anjaleenab@github.com>
2020-01-24 13:05:15 -0800

    styled the dashboard

--- client/components/dashboard.jsx
+++ client/components/dashboard.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 export default function Dashboard(props) {
   return (
     <main className="mainDashboard">
-      <div className="stat1"></div>
-      <div className="stat2"></div>
-      <div className="stat3"></div>
-      <div className="stat4"></div>
-      <div className="stat5"></div>
+      <div className="topDash">
+        <div className="stat1"></div>
+        <div className="stat2"></div>
+        <div className="stat3"></div>
+      </div>
+      <div className="bottomDash">
+        <div className="stat4"></div>
+        <div className="stat5"></div>
+      </div>
     </main>
   );
 }
