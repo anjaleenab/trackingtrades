c53f4daf808f662195fe190347d6e807ca397d30
anjaleena <anjaleenab@github.com>
2020-01-24 13:54:21 -0800

    changed the size of the text of the header conditionally

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default function Header(props) {
-  return (
-    <header className="header">
-      <h1 className="heading">Tracking Trades</h1>
-    </header>
-  );
+  if (props.loggedIn) {
+    return (
+      <header style={{ fontSize: '1.5em' }} className="header">
+        <h1 className="heading">Tracking Trades</h1>
+      </header>
+    );
+  } else {
+    return (
+      <header style={{ fontSize: '3.5em' }} className="header">
+        <h1 className="heading">Tracking Trades</h1>
+      </header>
+    );
+  }
+
 }
--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,35 +1,42 @@
 import React, { useState } from 'react';
+import Header from './header';
 import Dashboard from './dashboard';
 
 export default function LoginPage(props) {
   const [loggedIn, setloggedIn] = useState(false);
   if (!loggedIn) {
     return (
-
-      <div className="login">
-        <div className="login-image">
-        </div>
-        <div className="loginNecessities">
-          <div className="login-section">
-            <div className="login-input1">
-              <label>Username:</label>
-              <input></input>
-            </div>
-            <div className="login-input2">
-              <label>Password:</label>
-              <input></input>
-            </div>
+      <React.Fragment>
+        <Header status={loggedIn}/>
+        <div className="login">
+          <div className="login-image">
           </div>
-          <button
-            className="login-btn"
-            onClick={() => setloggedIn(!loggedIn)}
-          >Log In</button>
+          <div className="loginNecessities">
+            <div className="login-section">
+              <div className="login-input1">
+                <label>Username:</label>
+                <input></input>
+              </div>
+              <div className="login-input2">
+                <label>Password:</label>
+                <input></input>
+              </div>
+            </div>
+            <button
+              className="login-btn"
+              onClick={() => setloggedIn(!loggedIn)}
+            >Log In</button>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
+
     );
-  } else if (loggedIn) {
+  } else {
     return (
-      <Dashboard/>
+      <React.Fragment>
+        <Header status={loggedIn} />
+        <Dashboard/>
+      </React.Fragment>
     );
   }
 }
