ce20883af62ea9b79d05632cb007505e4484cfd2
anjaleena <anjaleenab@github.com>
2020-01-23 18:45:40 -0800

    styled the login section of the login component

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -4,15 +4,18 @@ export default function LoginPage(props) {
   return (
     <div className="login">
       <div className="login-image"></div>
-      <div className="login-section">
-        <div>
-          <label>Username:</label>
-          <input></input>
+      <div className="loginNecessities">
+        <div className="login-section">
+          <div className="login-input1">
+            <label>Username:</label>
+            <input></input>
+          </div>
+          <div className="login-input2">
+            <label>Password:</label>
+            <input></input>
+          </div>
         </div>
-        <div>
-          <label>Password:</label>
-          <input></input>
-        </div>
+        <button className="login-btn">Log In</button>
       </div>
     </div>
   );
