ce93d12ae2ab0e6d641d0554180811557e8c2c22
anjaleena <anjaleenab@github.com>
2020-01-24 10:42:30 -0800

    created the html elements for the dashboard: main, and 5 divs

--- client/components/dashboard.jsx
+++ client/components/dashboard.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function Dashboard(props) {
   return (
-    <div>This is the dashboard</div>
+    <main className="mainDashboard">
+      <div className="stat1"></div>
+      <div className="stat2"></div>
+      <div className="stat3"></div>
+      <div className="stat4"></div>
+      <div className="stat5"></div>
+    </main>
   );
 }
