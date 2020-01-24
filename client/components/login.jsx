5ff6371188d44170e119655aa0eecc110aa94e72
anjaleena <anjaleenab@github.com>
2020-01-23 21:40:26 -0800

    created the dashboard component, imported it from the login component and added a react hook to change the status from logged out to logged in

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dashboard from './dashboard';
 
 export default function LoginPage(props) {
-  return (
-    <div className="login">
-      <div className="login-image">
-      </div>
-      <div className="loginNecessities">
-        <div className="login-section">
-          <div className="login-input1">
-            <label>Username:</label>
-            <input></input>
-          </div>
-          <div className="login-input2">
-            <label>Password:</label>
-            <input></input>
-          </div>
+  const [loggedIn, setloggedIn] = useState(false);
+  if (!loggedIn) {
+    return (
+
+      <div className="login">
+        <div className="login-image">
         </div>
-        <button className="login-btn">Log In</button>
+        <div className="loginNecessities">
+          <div className="login-section">
+            <div className="login-input1">
+              <label>Username:</label>
+              <input></input>
+            </div>
+            <div className="login-input2">
+              <label>Password:</label>
+              <input></input>
+            </div>
+          </div>
+          <button
+            className="login-btn"
+            onClick={() => setloggedIn(!loggedIn)}
+          >Log In</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else if (loggedIn) {
+    return (
+      <Dashboard/>
+    );
+  }
 }
