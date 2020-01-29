b1c5e01f81e063aeff2274cfb18b6dda33367915
anjaleena <anjaleenab@github.com>
2020-01-29 15:11:06 -0800

    cleaned up code (got rid of comments and extraneous code

--- client/components/tradeinput.jsx
+++ client/components/tradeinput.jsx
@@ -2,36 +2,6 @@ import React from 'react';
 import TradeInputRow from './tradeInputRow';
 
 export default function TradeInput(props) {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     date: '',
-  //     stock: '',
-  //     quantity: '',
-  //     boughtAt: '',
-  //     soldAt: '',
-  //     pAndL: ''
-  //   };
-  //   this.change = this.change.bind(this);
-  // }
-  // change(event) {
-  //   this.setState({
-  //     [event.target.name]: event.target.value
-  //   });
-  //   console.log(event.target.name);
-  // }
-  // componentDidMount() {
-  //   let originalVal = this.props.trades[0];
-  //   this.setState({
-  //     date: originalVal.date,
-  //     stock: originalVal.stock,
-  //     quantity: originalVal.quantity,
-  //     boughtAt: originalVal.boughtAt,
-  //     soldAt: originalVal.soldAt,
-  //     pAndL: originalVal.pAndL
-  //   });
-  // }
-  // render() {
   return (
     props.trades.map(trade => {
       let id = trade['ID'];
@@ -45,8 +15,6 @@ export default function TradeInput(props) {
         <TradeInputRow key={id} date={date} stock={stock} quantity={quantity} boughtAt={boughtAt}
           soldAt={soldAt} pAndL={pAndL}/>
       );
-      // })
-    // );
-    }
-    ));
+    })
+  );
 }
