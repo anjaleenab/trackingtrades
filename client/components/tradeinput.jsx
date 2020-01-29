c8614dc02005efe04a7274af52fa5e1f6f6122f7
anjaleena <anjaleenab@github.com>
2020-01-28 19:02:37 -0800

    added a componentDidMount function to set the state of the input component when it mounts

--- client/components/tradeinput.jsx
+++ client/components/tradeinput.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 export default class TradeInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      date: '',
+      stock: '',
+      quantity: '',
+      boughtAt: '',
+      soldAt: '',
+      pAndL: ''
+    };
+    this.change = this.change.bind(this);
+  }
+  change(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  componentDidMount() {
+    let originalVal = this.props.trades;
+    this.setState({
+      date: originalVal.date,
+      stock: originalVal.stock,
+      quantity: originalVal.quantity,
+      boughtAt: originalVal.boughtAt,
+      soldAt: originalVal.soldAt,
+      pAndL: originalVal.pAndL
+    });
+    console.log('mounted here is test: ' + this.props.trades);
   }
   render() {
     return (
@@ -17,12 +42,18 @@ export default class TradeInput extends React.Component {
         let pAndL = trade['Profit-Loss'];
         return (
           <div className="data-row-input" key={id}>
-            <input id="dateInput" value={date}></input>
-            <input id="stockInput" value={stock}></input>
-            <input id="quantityInput" value={quantity}></input>
-            <input id="boughtInput" value={boughtAt}></input>
-            <input id="soldInput" value={soldAt}></input>
-            <input id="pAndLInput" value={pAndL}></input>
+            <input id="dateInput"
+              onChange={event => this.change(event)} type="text" name="date" value={this.state.date} ></input>
+            <input id="stockInput"
+              name="stock" value={this.state.stock} onChange={event => this.change(event)}></input>
+            <input id="quantityInput"
+              name="quantity" value={this.state.quantity} onChange={event => this.change(event)}></input>
+            <input id="boughtInput"
+              name="boughtAt" value={this.state.boughtAt} onChange={event => this.change(event)}></input>
+            <input id="soldInput"
+              name="soldAt" value={this.state.soldAt} onChange={event => this.change(event)}></input>
+            <input id="pAndLInput"
+              name="pAndL" value={this.state.pAndL} onChange={event => this.change(event)}></input>
           </div>
         );
       })
