62c3aaaba4a9a5c4e45b724acd4f4c5763ee6173
anjaleena <anjaleenab@github.com>
2020-02-02 15:16:46 -0800

    added links to the navbar component

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
     <div>
-      <button>Dashboard</button>
-      <button>Trade Log</button>
-      <button>Log Out</button>
+      <Link to="/dashboard"><button>Dashboard</button></Link>
+      <Link to="/tradeLog"><button>Trade Log</button></Link>
+      <Link to="/"><button>Log Out</button></Link>
     </div>
   );
 }
