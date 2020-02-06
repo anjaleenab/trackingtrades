94b808b68ca2e7138f79f6c446980a71667cb684
anjaleena <anjaleenab@github.com>
2020-02-05 18:23:40 -0800

    fixed a typo in the showError function

--- client/components/validate.jsx
+++ client/components/validate.jsx
@@ -8,10 +8,9 @@ export default function Validate(props, name) {
     event.target.closest('.data-row-input').setAttribute('className', 'error');
     showError();
     // let width = event.target.getAttribute('style');
-    // event.target.setAttribute('style', width + ' background-color: #FFA98F');
+    // event.target.setAttribute('style', width + ' backgroundColor: #FFA98F');
   } else {
     if (name === 'Date') {
-      debugger;
       const date = new Date();
       if (event.target.value.length === 10) {
         const values = event.target.value.split('');
@@ -24,22 +23,25 @@ export default function Validate(props, name) {
             if (validNum.test(number)) {
               return typeof (number);
             } else {
-              errorDiv.textContent = 'Date must be in date format Ex: 01-01-' + date.getFullYear() +
+              errorDiv.textContent += 'Date must be in date format Ex: 01-01-' + date.getFullYear() +
                 ' or Ex: 01/01/' + date.getFullYear();
+              showError();
             }
           }
         });
         let valid = ['number', 'number', 'string', 'number', 'number', 'string', 'number', 'number', 'number', 'number'];
         if (numbers.join('') !== valid.join('')) {
-          errorDiv.textContent = 'Date must be in date format Ex: 01-01-' + date.getFullYear() +
+          errorDiv.textContent += 'Date must be in date format Ex: 01-01-' + date.getFullYear() +
             ' or Ex: 01/01/' + date.getFullYear();
+          showError();
+        } else {
+          errorDiv.textContent = '';
         }
-      } else if (event.target.value.length < 10 || event.target.value > 10) {
-        errorDiv.textContent = 'Date must be in date format Ex: 01-01-' + date.getFullYear() +
+      } else if (event.target.value.length !== 10) {
+        console.log(event.target.value.length);
+        errorDiv.textContent += 'Date must be in date format Ex: 01-01-' + date.getFullYear() +
           ' or Ex: 01/01/' + date.getFullYear();
         showError();
-      } else {
-        errorDiv.textContent = '';
       }
       console.log('here');
       console.log(event.target.closest('.data-row-input'));
@@ -50,5 +52,6 @@ export default function Validate(props, name) {
 
 function showError() {
   let width = event.target.getAttribute('style');
-  event.target.setAttribute('style', width + ' background-color: #FFA98F');
+  // console.log(event.target.attribute());
+  event.target.setAttribute('style', width + 'background-color: #FFA98F;');
 }
