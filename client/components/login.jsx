6a3f20b208d0be7759d1ea489c74f6ebf71011e1
anjaleena <anjaleenab@github.com>
2020-03-06 15:53:12 -0800

    added the ternary to conditionally render the sign up button

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -4,7 +4,7 @@ import { Redirect } from 'react-router-dom';
 
 export default function LoginPage(props) {
   const [loggedIn, setloggedIn] = useState(false);
-  const [notSignedUp, setSignedUp] = useState(false);
+  const [signUp, setSignUp] = useState(false);
   if (!loggedIn) {
     return (
       <React.Fragment>
@@ -23,27 +23,33 @@ export default function LoginPage(props) {
                 <input></input>
               </div>
             </div>
-            <div className="landing-buttons">
-              <button
-                className="login-btn"
-                onClick={() => {
-                  setloggedIn(!loggedIn);
-                }}
-              >Log In</button>
-              <button
+            {!signUp
+              ? <div className="landing-buttons">
+                <button
+                  className="login-btn"
+                  onClick={() => {
+                    setloggedIn(!loggedIn);
+                  }}
+                >Log In</button>
+                <button
+                  className="sign-in-btn"
+                  onClick={() => {
+                    setSignUp(!signUp);
+                  }}
+                >Sign Up</button>
+              </div>
+              : <button
                 className="sign-in-btn"
                 onClick={() => {
-                  setloggedIn(!notSignedUp);
+                  setSignUp(!signUp);
                 }}
-              >Sign Up</button>
-            </div>
+              >Sign Up!</button>
+            }
           </div>
         </div>
       </React.Fragment>
     );
-    // } else if (notSignedUp) {
 
-  // }
   } else {
     return (
       <React.Fragment>
