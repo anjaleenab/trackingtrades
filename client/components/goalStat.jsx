95ef50df7820b36ab253ea71dd8f939d88583f3f
anjaleena <anjaleenab@github.com>
2020-03-10 11:54:17 -0700

    added some styling to the goalPercent stat

--- client/components/goalStat.jsx
+++ client/components/goalStat.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function GoalStat(props) {
   const [goalMode, setGoalMode] = useState(false);
-  return (
-    null
-  );
+  if (goalMode) {
+    return (
+      null
+    );
+  } else {
+    return (
+      <div className="goalPercentDiv">
+      You have reached 1% of your goal of $400 this week
+        <div>
+          <button className="setGoal-btn" onClick={() => setGoalMode(true)}>Set New Goal</button>
+        </div>
+      </div>
+    );
+  }
+
 }
